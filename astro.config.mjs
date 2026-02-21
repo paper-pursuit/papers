604356d60be116797dbc987f2df3ddfc0d991086
Mohit Karekar <mohit.karekar@zalando.de>
2026-02-21 14:45:24 +0100

    Astro init

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,22 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import tailwindcss from "@tailwindcss/vite";
+
+import react from '@astrojs/react';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [react()],
+  vite: {
+    ssr: {
+      noExternal: ['@primer/react']
+    },
+    plugins: [tailwindcss()],
+    build: {
+      sourcemap: false
+    },
+    css: {
+      devSourcemap: false
+    }
+  }
+});
